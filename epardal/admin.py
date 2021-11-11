a143c688ce59c4b3795a3dc4dfdcdb65b4ae1901
Edmilbe Ramos <edmilbe@gmail.com>
2021-11-11 15:38:50 +0000

    [migrate] add Equipment to Admin

--- epardal/admin.py
+++ epardal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
+from epardal.models import Equipment
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color','height')
